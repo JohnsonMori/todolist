f6b5b5ec7337c5691faf5ec94ea4305d4dad3103
JohnsonMori <960612296@qq.com>
2021-06-15 20:53:23 +0800

    feat: ref single data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
+  <!-- <div>
     <nav-header></nav-header>
     <nav-main></nav-main>
     <nav-footer></nav-footer>
+  </div> -->
+
+  <div>
+    {{num}}
+  </div>
+  <div>
+    {{name}}
+  </div>
+  <div>
+    {{arr}}
+  </div>
+  <div>
+    {{obj.age}}
   </div>
 </template>
 
@@ -10,7 +23,7 @@
 import NavHeader from '@/components/navHeader/NavHeader'
 import NavMain from '@/components/navMain/NavMain'
 import NavFooter from '@/components/navFooter/NavFooter'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'Home',
@@ -20,7 +33,18 @@ export default defineComponent({
     NavFooter
   },
   setup() {
-
+    let num = ref(10)
+    let name = ref('johnson')
+    let arr = ref(['a', 'b', 'c', 'd'])
+    let obj = ref({
+      age: 20
+    })
+    return {
+      num,
+      name,
+      arr,
+      obj
+    }
   }
 })
 </script>
