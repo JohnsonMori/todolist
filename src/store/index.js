998e0f387a1ca2381dc9f24ceb786d1f97a2c711
JohnsonMori <960612296@qq.com>
2021-06-16 12:37:47 +0800

    feat: vuex store management

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    // 定义所需要的状态的
+    state: {
+        name: 'johnson',
+    },
+    // 同步修改state 都是方法
+    // 第一个参数state 第二个参数是需要修改的值
+    mutations: {
+        setName(state, payload) {
+            state.name = payload
+            // state.name.slice(0, 1)
+        },
+    },
+    // 异步提交mutation
+    // 第一个参数是store 第二个参数是修改的值
+    actions: {
+        asyncSetName(store, params) {
+            setTimeout(() => {
+                // commit是提交mutation 调用mutation的方法
+                store.commit('setName', params)
+            }, 2000)
+        },
+    },
+    // 模块化
+    modules: {},
 })
