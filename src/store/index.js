914883d6bc0128165e2a2b171b7350fa3d5bb1aa
JohnsonMori <960612296@qq.com>
2021-06-16 20:03:54 +0800

    feat: todolist mutations

--- src/store/index.js
+++ src/store/index.js
@@ -3,26 +3,41 @@ import { createStore } from 'vuex'
 export default createStore({
     // 定义所需要的状态的
     state: {
-        name: 'johnson',
+        list: [
+            {
+                title: '吃饭',
+                complete: false,
+            },
+            {
+                title: '睡觉',
+                complete: false,
+            },
+            {
+                title: '敲代码',
+                complete: true,
+            },
+        ],
     },
     // 同步修改state 都是方法
     // 第一个参数state 第二个参数是需要修改的值
     mutations: {
-        setName(state, payload) {
-            state.name = payload
-            // state.name.slice(0, 1)
+        // 添加任务
+        addTodo(state, payload) {
+            state.list.push(payload)
+        },
+        // 删除任务 splice(下标，个数)
+        delTodo(state, payload) {
+            state.list.splice(payload, 1)
+        },
+        // 清除已完成
+        clear(state, payload) {
+            // 把过滤之后的数组传进来
+            state.list = payload
         },
     },
     // 异步提交mutation
     // 第一个参数是store 第二个参数是修改的值
-    actions: {
-        asyncSetName(store, params) {
-            setTimeout(() => {
-                // commit是提交mutation 调用mutation的方法
-                store.commit('setName', params)
-            }, 2000)
-        },
-    },
+    actions: {},
     // 模块化
     modules: {},
 })
