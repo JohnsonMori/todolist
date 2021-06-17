0b598f41988b8a0406535affc55528fc78ab1aa8
JohnsonMori <960612296@qq.com>
2021-06-17 21:07:54 +0800

    feat: vuex store data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <!-- <nav-header></nav-header>
+    <nav-header></nav-header>
     <nav-main></nav-main>
-    <nav-footer></nav-footer> -->
+    <nav-footer></nav-footer>
     <div>
+      {{list}}
+    </div>
+    <!-- <div>
       {{num1}} --- {{num2}}
       两个数的和: {{addNum}}
     </div>
     <div>
       <button @click="add">add</button>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -18,6 +21,7 @@ import NavHeader from '@/components/navHeader/NavHeader'
 import NavMain from '@/components/navMain/NavMain'
 import NavFooter from '@/components/navFooter/NavFooter'
 import { defineComponent, ref, reactive, toRefs, computed } from 'vue'
+import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'Home',
@@ -27,23 +31,29 @@ export default defineComponent({
     NavFooter
   },
   setup() {
-    let num1 = ref(10)
-    let num2 = ref(20)
-    let addNum = computed(() => {
-      // 必须返回一个值
-      // 逻辑代码
-      // 购物车总价
-      return num1.value + num2.value
+    let store = useStore()
+    let list = computed(() => {
+      return store.state.list
     })
-    let add = () => {
-      num1.value ++
-      num2.value ++
-    }
+    // console.log(store)
+    // let num1 = ref(10)
+    // let num2 = ref(20)
+    // let addNum = computed(() => {
+    //   // 必须返回一个值
+    //   // 逻辑代码
+    //   // 购物车总价
+    //   return num1.value + num2.value
+    // })
+    // let add = () => {
+    //   num1.value ++
+    //   num2.value ++
+    // }
     return {
-      num1,
-      num2,
-      addNum,
-      add
+      // num1,
+      // num2,
+      // addNum,
+      // add
+      list
     }
   }
 })
