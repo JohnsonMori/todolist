eaee2ea52abbfddc629fecf20df9155db790f68e
JohnsonMori <960612296@qq.com>
2021-06-18 11:38:16 +0800

    feat: vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+// 路由的配置数组
+// path: 路由路径 必须以/开头
+// component: 对应的路由组件
+// name: 路由名字
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // 按需引入
+        // 如果没有访问/about 就不会加载这个组件 节约性能
+        component: () => import('../views/About.vue'),
+    },
+    {
+        path: '/detail',
+        name: 'Detail',
+        component: () => import('../views/Detail.vue'),
+    },
 ]
 
+// 创建路由对象
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    // history: createWebHashHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 })
 
 export default router
---
+++ src/views/Detail.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="detail">
+    <h1>这是detail组件</h1>
+  </div>
+</template>
